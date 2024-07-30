972d79847f8561fc61adb66423905214f6317b60
Shashank Hondad <you@example.com>
2024-07-30 20:20:11 +0530

    Class And Object Bill Program Code

---
+++ Bill.java
@@ -0,0 +1,14 @@
+class Bill{
+		
+		
+		int billReferenceId;
+		int billSequenceNo;
+		String billIssueDate;
+		String billDueDate;
+		boolean isBillPaid;
+		boolean isLatestBill;
+		boolean isFinalBill;
+		double billAmount;
+		boolean taxInclusiveBill;
+		}
+	
---
+++ BillRunner.java
@@ -0,0 +1,339 @@
+class BillRunner{
+	
+	public static void main(String []bills){
+		
+		System.out.println("--The Bill Reference Id Numbers are---- : ");
+
+		Bill ref = new Bill();
+	int billReferenceId = ref.billReferenceId;
+	   System.out.println(billReferenceId);
+	   
+	   billReferenceId = 505020;
+System.out.println(billReferenceId);
+	   
+	   billReferenceId = 505021;
+System.out.println(billReferenceId);
+
+  billReferenceId = 505022;
+System.out.println(billReferenceId);
+
+  billReferenceId = 505023;
+System.out.println(billReferenceId);
+
+  billReferenceId = 505024;
+System.out.println(billReferenceId);
+
+  billReferenceId = 505025;
+System.out.println(billReferenceId);
+
+  billReferenceId = 505026;
+System.out.println(billReferenceId);
+
+  billReferenceId = 505027;
+System.out.println(billReferenceId);
+
+  billReferenceId = 505028;
+System.out.println(billReferenceId);
+
+  billReferenceId = 505029;
+System.out.println(billReferenceId);
+
+System.out.println("--The Bill Sequence  Numbers are ----  :");
+
+		Bill ref1 = new Bill();
+	int billSequenceNo = ref1.billSequenceNo;
+	   System.out.println(billSequenceNo);   
+	   
+	   billSequenceNo = 101;
+System.out.println(billSequenceNo);
+
+billSequenceNo = 102;
+System.out.println(billSequenceNo);
+
+billSequenceNo = 103;
+System.out.println(billSequenceNo);
+
+billSequenceNo = 104;
+System.out.println(billSequenceNo);
+
+billSequenceNo = 105;
+System.out.println(billSequenceNo);
+
+billSequenceNo = 106;
+System.out.println(billSequenceNo);
+
+billSequenceNo = 107;
+System.out.println(billSequenceNo);
+
+billSequenceNo = 108;
+System.out.println(billSequenceNo);
+
+billSequenceNo = 109;
+System.out.println(billSequenceNo);
+
+billSequenceNo = 110;
+System.out.println(billSequenceNo);
+
+System.out.println("--The Bill Issue dates are ----  :");
+
+		Bill ref2 = new Bill();
+	String billIssueDate = ref2.billIssueDate;
+	   System.out.println(billIssueDate);  
+	   
+	 billIssueDate = "21/07/2024";
+	 System.out.println(billIssueDate);
+	 
+	  billIssueDate = "22/07/2024";
+	 System.out.println(billIssueDate);
+	 
+	  billIssueDate = "23/07/2024";
+	 System.out.println(billIssueDate);
+	 
+	  billIssueDate = "21/07/2024";
+	 System.out.println(billIssueDate);
+	 
+	  billIssueDate = "24/07/2024";
+	 System.out.println(billIssueDate);
+	 
+	  billIssueDate = "25/07/2024";
+	 System.out.println(billIssueDate);
+	 
+	  billIssueDate = "26/07/2024";
+	 System.out.println(billIssueDate);
+	 
+	  billIssueDate = "27/07/2024";
+	 System.out.println(billIssueDate);
+	 
+	  billIssueDate = "28/07/2024";
+	 System.out.println(billIssueDate);
+	 
+	  billIssueDate = "29/07/2024";
+	 System.out.println(billIssueDate);
+	 
+	  billIssueDate = "30/07/2024";
+	 System.out.println(billIssueDate);
+
+  System.out.println("--The Bill Due dates are ----  :");
+
+		Bill ref3 = new Bill();
+	String billDueDate = ref3.billDueDate;
+	   System.out.println(billDueDate); 
+	   
+	   billDueDate = "10/8/2024";
+	   System.out.println(billDueDate);
+	   
+	   billDueDate = "11/8/2024";
+	   System.out.println(billDueDate);
+	   
+	   billDueDate = "12/8/2024";
+	   System.out.println(billDueDate);
+	   
+	   billDueDate = "13/8/2024";
+	   System.out.println(billDueDate);
+	   
+	   billDueDate = "14/8/2024";
+	   System.out.println(billDueDate);
+	   
+	   billDueDate = "15/8/2024";
+	   System.out.println(billDueDate);
+	   
+	   billDueDate = "16/8/2024";
+	   System.out.println(billDueDate);
+	   
+	   billDueDate = "17/8/2024";
+	   System.out.println(billDueDate);
+	   
+	   billDueDate = "18/8/2024";
+	   System.out.println(billDueDate);
+	   
+	   billDueDate = "19/8/2024";
+	   System.out.println(billDueDate);
+	   
+	   System.out.println("--If The Bill is Paid or Not ----  :");
+
+		Bill ref4 = new Bill();
+	boolean isBillPaid = ref4.isBillPaid;
+	   System.out.println(isBillPaid); 
+	   
+	   isBillPaid = true;
+	   System.out.println(isBillPaid);
+	   
+	    isBillPaid = true;
+	   System.out.println(isBillPaid);
+	   
+	    isBillPaid = false;
+	   System.out.println(isBillPaid);
+	   
+	    isBillPaid = true;
+	   System.out.println(isBillPaid);
+	   
+	    isBillPaid = false;
+	   System.out.println(isBillPaid);
+	   
+	    isBillPaid = false;
+	   System.out.println(isBillPaid);
+	   
+	    isBillPaid = true;
+	   System.out.println(isBillPaid);
+	   
+	    isBillPaid = true;
+	   System.out.println(isBillPaid);
+	   
+	    isBillPaid = false;
+	   System.out.println(isBillPaid);
+	   
+	    isBillPaid = true;
+	   System.out.println(isBillPaid);
+	   
+	   System.out.println("--The Bill Is Latest one ----  :");
+
+		Bill ref5 = new Bill();
+	boolean isLatestBill = ref5.isLatestBill;
+	   System.out.println(isLatestBill);  
+	   
+	   isLatestBill = true;
+	   System.out.println(isLatestBill);
+	   
+	   isLatestBill = true;
+	   System.out.println(isLatestBill);
+	   
+	   isLatestBill = true;
+	   System.out.println(isLatestBill);
+	   
+	   isLatestBill = false;
+	   System.out.println(isLatestBill);
+	   
+	   isLatestBill = false;
+	   System.out.println(isLatestBill);
+	   
+	   isLatestBill = true;
+	   System.out.println(isLatestBill);
+	   
+	   isLatestBill = true;
+	   System.out.println(isLatestBill);
+	   
+	   isLatestBill = false;
+	   System.out.println(isLatestBill);
+	   
+	   isLatestBill = true;
+	   System.out.println(isLatestBill);
+	   
+	   isLatestBill = true;
+	   System.out.println(isLatestBill);
+	   
+	   System.out.println("--The Final Bills Are  ----  :");
+	   
+	   Bill ref6 = new Bill();
+	boolean isFinalBill = ref6.isFinalBill;
+	   System.out.println(isFinalBill);  
+	   
+	   isFinalBill = true;
+	   System.out.println(isFinalBill);
+	   
+	   isFinalBill = true;
+	   System.out.println(isFinalBill);
+	   
+	   isFinalBill = true;
+	   System.out.println(isFinalBill);
+	   
+	   isFinalBill = true;
+	   System.out.println(isFinalBill);
+	   
+	   isFinalBill = false;
+	   System.out.println(isFinalBill);
+	   
+	   isFinalBill = true;
+	   System.out.println(isFinalBill);
+	   
+	   isFinalBill = true;
+	   System.out.println(isFinalBill);
+	   
+	   isFinalBill = false;
+	   System.out.println(isFinalBill);
+	   
+	   isFinalBill = true;
+	   System.out.println(isFinalBill);
+	   
+	   isFinalBill = true;
+	   System.out.println(isFinalBill);
+	   
+	    System.out.println("--The Bill Amouts Are  ----  :");
+	   
+	   Bill ref7 = new Bill();
+	double billAmount = ref7.billAmount;
+	   System.out.println(billAmount);  
+	   
+	   billAmount = 20999.00d;
+	   System.out.println(billAmount);
+	   
+	    billAmount = 25999.00d;
+	   System.out.println(billAmount);
+	   
+	    billAmount = 36990.00d;
+	   System.out.println(billAmount);
+	   
+	    billAmount = 39899.85d;
+	   System.out.println(billAmount);
+	   
+	    billAmount = 120900.00d;
+	   System.out.println(billAmount);
+	   
+	    billAmount = 150999.50d;
+	   System.out.println(billAmount);
+	   
+	    billAmount = 180998.50d;
+	   System.out.println(billAmount);
+	   
+	    billAmount = 200999.00d;
+	   System.out.println(billAmount);
+	   
+	    billAmount = 210900.05d;
+	   System.out.println(billAmount);
+	   
+	    billAmount = 220999.50d;
+	   System.out.println(billAmount);
+	   
+	   
+	   System.out.println("--The Tax Inclusive Bills Are  ----  :");
+	   
+	   Bill ref8 = new Bill();
+	boolean taxInclusiveBill = ref8.taxInclusiveBill;
+	   System.out.println(taxInclusiveBill);  
+	   
+	   taxInclusiveBill = true;
+	   System.out.println(taxInclusiveBill);
+	   
+	   taxInclusiveBill = true;
+	   System.out.println(taxInclusiveBill);
+	   
+	   taxInclusiveBill = true;
+	   System.out.println(taxInclusiveBill);
+	   
+	   taxInclusiveBill = true;
+	   System.out.println(taxInclusiveBill);
+	   
+	   taxInclusiveBill = false;
+	   System.out.println(taxInclusiveBill);
+	   
+	   taxInclusiveBill = true;
+	   System.out.println(taxInclusiveBill);
+	   
+	   taxInclusiveBill = true;
+	   System.out.println(taxInclusiveBill);
+	   
+	   taxInclusiveBill = true;
+	   System.out.println(taxInclusiveBill);
+	   
+	   taxInclusiveBill = true;
+	   System.out.println(taxInclusiveBill);
+	   
+	   taxInclusiveBill = false;
+	   System.out.println(taxInclusiveBill);
+	   
+	   
+	   
+	   
+	   
+
+}
+}
